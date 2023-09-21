e459873afbd5476af5d5f9e0b6cbe5e762322a17
Source Academy User <No public email provided>
2023-09-21 15:26:51 +0800

    Changes made from Source Academy

--- reflection6.js
+++ reflection6.js
@@ -18,11 +18,11 @@ function insertion_sort_cmp(xs, cmp) {
 const xs = list(6, 3, 8, 5, 1, 9, 6, 4, 2, 7);
 //const xs = list(6,3,8);
 // (a)
-//insertion_sort_cmp(xs, (x, y) => x < y);
+//insertion_sort_cmp(xs, (x, y) => x <= y);
 // Result: list(1, 2, 3, 4, 5, 6, 6, 7, 8, 9)
 
 // (b)
-insertion_sort_cmp(xs, (x, y) => y < x);
+insertion_sort_cmp(xs, (x, y) => y <= x);
 // Result: list(9, 8, 7, 6, 6, 5, 4, 3, 2, 1)
 
 // (c)
@@ -39,10 +39,10 @@ const even_first_and_sorted_cmp = (x, y) => {
     return false;
   }
   else if (x % 2 === 0 && y % 2 === 0) {
-    return x < y;
+    return x <= y;
   }
   else {
-    return y < x;
+    return y <= x;
   }
 };
 
@@ -50,6 +50,8 @@ insertion_sort_cmp(xs, even_first_and_sorted_cmp);
 // Result: list(2, 4, 6, 6, 8, 9, 7, 5, 3, 1)
 
 /*
+
+// Question 2
 
 Consider the partial implementation of merge_sort shown in class:
 // half, rounded downwards
@@ -102,5 +104,13 @@ Characterize the running time of merge_sort, using the big-O, Θ or Ω notation.
 
 
 /*
-complexity of merge is 
+complexity of merge is O(n).
+
+Complexity of merge_sort is O(n) *O(logn) = O(nlogn)
 */
+
+function take(xs, n) {
+    return n === 0 ? null : pair(head(xs), take(tail(xs), n-1));
+}
+
+// Homework: sort it with odd even alternating. odd numbers are ascneding and even are descending
