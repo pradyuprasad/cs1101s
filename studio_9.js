de846e3ebe0b885ea80af977ca6a2e83a49d8b53
Source Academy User <No public email provided>
2023-10-15 16:54:51 +0800

    Changes made from Source Academy

--- studio_9.js
+++ studio_9.js
@@ -1,18 +1,22 @@
 function d_filter(pred, xs) {
-    if (!is_null(xs)) {
-        if (pred(head(xs))) {
+    if (is_null(xs)) {
+        // do nothing
+    }
+    else {
+                if (pred(head(xs))) {
             set_tail(xs, d_filter(pred, tail(xs)));
         }
         else {
-            // unsure
+            d_filter(pred, tail(xs));
         }
-    }
-    else {
-        // do nothing 
+
         }
     
     
 }
+
+
+
 
 const L = list(1, 2, 3, 4, 5, 6, 7, 8, 9, 11);
 d_filter(x => x % 2 === 0, L); // returns [2, [4, [6, [8, null]]]]
